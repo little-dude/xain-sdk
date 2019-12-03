ad3778c09d140fea2bc1014ce00aaf4510b90108
janpetschexain <58227040+janpetschexain@users.noreply.github.com>
2019-12-03 14:13:11 +0100

    XP-209 create keras participant example (#5)
    * XP-209 create keras participant example
    
    * XP-209 resolve review threads

--- xain_sdk/sdk/participant.py
+++ xain_sdk/sdk/participant.py
@@ -1,4 +1,10 @@
 """Provides participant API"""
+
+from abc import ABC, abstractmethod
+from typing import Dict, List, Tuple
+
+import numpy as np
+
 from xain_sdk.sdk.use_case import UseCase
 
 
@@ -12,3 +18,30 @@ def start(coordinator_url: str, use_case: UseCase):
     """
 
     raise NotImplementedError
+
+
+class Participant(ABC):
+    """An abstract participant for federated learning."""
+
+    @abstractmethod
+    def train_round(
+        self, weights: List[np.ndarray], epochs: int, epoch_base: int
+    ) -> Tuple[List[np.ndarray], Dict[str, List[np.ndarray]]]:
+        # pylint: disable=line-too-long
+        """Train the model in a federated learning round.
+
+        A global model is given in terms of its `weights` and it is trained on local data for a
+        number of `epochs`. The weights of the updated local model are returned together with a set
+        of metrics.
+
+        Args:
+            weights (~typing.List[~numpy.ndarray]): The weights of the global model.
+            epochs (int): The number of epochs to be trained.
+            epoch_base (int): The epoch base number for the optimizer state (in case of epoch
+                dependent optimizer parameters).
+
+        Returns:
+            ~typing.Tuple[~typing.List[~numpy.ndarray], ~typing.Dict[str, ~typing.List[~numpy.ndarray]]]:
+                The updated model weights and the gathered metrics.
+        """
+        # pylint: enable=line-too-long
